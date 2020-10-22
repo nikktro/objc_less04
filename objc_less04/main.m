304cc0aabcbd2c33a8131e976ad8021d24803f50
Nikolay Trofimov <nikktro@gmail.com>
2020-10-22 12:43:32 +0300

    Initial Commit

---
+++ objc_less04/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  objc_less04
+//
+//  Created by Nikolay Trofimov on 22.10.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
